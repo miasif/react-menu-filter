e610719b5faf001e486991f3b05a30a3dbd0ed9a
miasif <minhazul.islam.asif@gmail.com>
2023-05-06 04:06:22 +0600

    done

---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Menu from './menuApi.js';
+import Navbar from './Navbar.jsx';
+import MenuCard from './MenuCard.jsx';
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  'All',
+];
+
+function Home() {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === 'All') {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar({ filterItem, menuList }) {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem) => {
+            return (
+              <button
+                key={curElem.id}
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}
+              >
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
